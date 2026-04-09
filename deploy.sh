a79ba8b09cdb12b6bd802b8d5dad199030a25cc5
Subhan Riaz <ssbhanriaz@gmail.com>
2026-04-09 07:02:28 +0500

    changes to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 set -e
 
-KEY="$HOME/.ssh/web-server-key.pem"
+KEY="/var/lib/jenkins/.ssh/ec2-key.pem"
 HOST="ubuntu@3.231.50.180"
 REMOTE_DIR="/var/www/portfolio"
 
 echo "Cleaning remote directory..."
-ssh -i  "$KEY" -o StrictHostKeyChecking=no $HOST "sudo rm -rf $REMOTE_DIR/*"
+ssh -i "$KEY" -o StrictHostKeyChecking=no $HOST "sudo rm -rf $REMOTE_DIR/*"
 
-echo "Copying files..."
-
+echo "Copying website files..."
 scp -i "$KEY" -o StrictHostKeyChecking=no index.html $HOST:$REMOTE_DIR/
-
 scp -i "$KEY" -o StrictHostKeyChecking=no -r images $HOST:$REMOTE_DIR/
 
-
-echo "Deployment done 🚀"
+echo "Deployment done."
