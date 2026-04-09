b467cd16c370214a34c8c2af5dd573f21ee69077
Subhan Riaz <ssbhanriaz@gmail.com>
2026-04-09 08:37:44 +0500

    updated index.html

--- deploy.sh
+++ deploy.sh
@@ -7,10 +7,9 @@ HOST="ubuntu@3.231.50.180"
 REMOTE_DIR="/var/www/portfolio"
 
 echo "Cleaning remote directory..."
-ssh -i "$KEY" -o StrictHostKeyChecking=no $HOST "sudo rm -rf $REMOTE_DIR/*"
+ssh -i "$KEY" -o StrictHostKeyChecking=no $HOST "sudo rm -rf $REMOTE_DIR/index.html"
 
 echo "Copying website files..."
 scp -i "$KEY" -o StrictHostKeyChecking=no index.html $HOST:$REMOTE_DIR/
-scp -i "$KEY" -o StrictHostKeyChecking=no -r images $HOST:$REMOTE_DIR/
 
 echo "Deployment done."
